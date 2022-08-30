a1cf6050104111d1781c1f0c9c2287bec83b1626
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-08-29 21:57:39 -0400

    fetching data from api

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/ProductList.scss'
 import { ProductItem } from "../components/ProductItem";
+import axios from "axios";
+
+const API_URL = 'https://api.escuelajs.co/api/v1/products'
 
 export const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(async() => {
+    const resp = await axios(API_URL)
+    setProducts(resp.data)
+  }, []);
+
   return (
     <section className="main-container">
       <div className="cards-container">
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        {products.map(product => {
+          return <ProductItem />
+        })}
       </div>
     </section>
   );
